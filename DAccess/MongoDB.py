c79bbf6a6c9c48c84d90d2baa69798e03ec9258c
diegopajarito <diegopajarito@gmail.com>
2016-03-09 00:45:01 +0100

    First Terminal Commit

---
+++ DAccess/MongoDB.py
@@ -0,0 +1,10 @@
+
+user = 'pajarito'
+password = 'D13g0GEOC*'
+serverUrl = 'geo-c6.init.uji.es'
+port = ''  #Default 27018
+
+if __name__ == "__main__":
+    print("Your MongoDB Config Values are here")
+
+#
