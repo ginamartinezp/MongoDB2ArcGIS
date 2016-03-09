45db2b2c812cf06bc48799a22349dc35d9b64ef3
diegopajarito <diegopajarito@gmail.com>
2016-03-09 01:20:16 +0100

    Defining ignore files

---
+++ DAccess/MongoDB.py
@@ -0,0 +1,11 @@
+#this is a reference File
+#To use this file fill the following authentication variables and
+#remove the '_Example' from file name, it is necessary to use this tool
+
+user = 'your_mongodb_username'
+password = 'your_mongodb_password'
+serverUrl = 'your_server_name_or_ip'
+port = ''  #Default 27018
+
+if __name__ == "__main__":
+    print("Your MongoDB Config Values are here")
